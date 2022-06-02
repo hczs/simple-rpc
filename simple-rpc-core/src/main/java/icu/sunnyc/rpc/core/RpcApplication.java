4f8c78a6ae31a7480e590dbd22d0a831a5262526
houcheng <houcheng@boco.com.cn>
2022-06-02 20:21:04 +0800

    :sparkles: 客户端可声明式调用，屏蔽创建代理等操作

--- simple-rpc-core/src/main/java/icu/sunnyc/rpc/core/RpcApplication.java
+++ simple-rpc-core/src/main/java/icu/sunnyc/rpc/core/RpcApplication.java
@@ -2,14 +2,24 @@ package icu.sunnyc.rpc.core;
 
 
 import icu.sunnyc.rpc.core.annotation.ServiceScan;
+import icu.sunnyc.rpc.core.annotation.SimpleReference;
+import icu.sunnyc.rpc.core.client.RpcProxy;
 import icu.sunnyc.rpc.core.constant.CommonConstant;
 import icu.sunnyc.rpc.core.server.RpcServer;
 import icu.sunnyc.rpc.core.spring.RpcServiceBeanRegistryProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.core.type.filter.TypeFilter;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Set;
 
 /**
  * RPC 启动类
@@ -26,13 +36,16 @@ public class RpcApplication {
         // 默认扫描 rpc core 包下的所有类
         String[] basePackages = new String[]{RpcApplication.class.getPackage().getName()};
         basePackages = addServicePackages(cls, basePackages);
-        // 手动扫描包并注册 bean
+        // 启动 Spring
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        RpcServiceBeanRegistryProcessor registryProcessor = new RpcServiceBeanRegistryProcessor(basePackages);
-        applicationContext.addBeanFactoryPostProcessor(registryProcessor);
-        applicationContext.refresh();
-        log.info("下面打印 Sring 的 Bean 列表");
-        Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(log::info);
+        // 注册 Bean
+        registerRpcAndServiceBean(applicationContext, basePackages);
+        // 注入客户端代理对象
+        try {
+            injectClientProxy(applicationContext);
+        } catch (Exception e) {
+            log.error("Inject client proxy error", e);
+        }
         // 启动 RPC 服务
         RpcServer rpcServer = applicationContext.getBean(RpcServer.class);
         rpcServer.start(applicationContext);
@@ -55,6 +68,40 @@ public class RpcApplication {
         String[] servicePackages = cls.getAnnotation(ServiceScan.class).basePackages();
         log.info("Get service scan package path：{}", Arrays.toString(servicePackages));
         return ArrayUtils.addAll(basePackages, servicePackages);
+    }
+
+    /**
+     * 注册 RPC 和 应用中 Rpc Service Bean
+     * @param applicationContext AnnotationConfigApplicationContext
+     * @param basePackages 扫描包
+     */
+    private static void registerRpcAndServiceBean(AnnotationConfigApplicationContext applicationContext, String[] basePackages) {
+        RpcServiceBeanRegistryProcessor registryProcessor = new RpcServiceBeanRegistryProcessor(basePackages);
+        applicationContext.addBeanFactoryPostProcessor(registryProcessor);
+        applicationContext.refresh();
+        log.info("下面打印 Spring 的 Bean 列表");
+        Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(log::info);
+    }
+
+    /**
+     * 注入客户端代理对象
+     * @param applicationContext AnnotationConfigApplicationContext
+     * @throws IllegalAccessException IllegalAccessException
+     */
+    private static void injectClientProxy(AnnotationConfigApplicationContext applicationContext) throws IllegalAccessException {
+        RpcProxy rpcProxy = applicationContext.getBean(RpcProxy.class);
+        String[] beans = applicationContext.getBeanDefinitionNames();
+        for (String bean : beans) {
+            Class<?> beanClass = applicationContext.getType(bean);
+            assert beanClass != null;
+            Field[] declaredFields = beanClass.getDeclaredFields();
+            for (Field declaredField : declaredFields) {
+                if (declaredField.isAnnotationPresent(SimpleReference.class)) {
+                    declaredField.setAccessible(true);
+                    declaredField.set(applicationContext.getBean(bean), rpcProxy.create(declaredField.getType()));
+                }
+            }
+        }
     }
 
 }
--- simple-rpc-demo/simple-rpc-demo-consumer/src/main/java/icu/sunnyc/rpc/demo/consumer/ConsumerApplication.java
+++ simple-rpc-demo/simple-rpc-demo-consumer/src/main/java/icu/sunnyc/rpc/demo/consumer/ConsumerApplication.java
@@ -2,21 +2,32 @@ package icu.sunnyc.rpc.demo.consumer;
 
 
 import icu.sunnyc.rpc.core.RpcApplication;
+import icu.sunnyc.rpc.core.annotation.SimpleReference;
 import icu.sunnyc.rpc.core.client.RpcProxy;
 import icu.sunnyc.rpc.demo.api.HelloService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
 /**
  * RPC 调用测试
  * @author: houcheng
  * @date: 2022/6/1 16:43:05
  */
+@Component
 public class ConsumerApplication {
+
+    @SimpleReference
+    private HelloService helloService;
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = RpcApplication.run(ConsumerApplication.class);
-        RpcProxy rpcProxy = context.getBean(RpcProxy.class);
-        HelloService helloService = rpcProxy.create(HelloService.class);
+        ConsumerApplication consumerApplication = context.getBean(ConsumerApplication.class);
+        consumerApplication.sayHello();
+    }
+
+    public void sayHello() {
         System.out.println(helloService.hello("sunnyc"));
     }
+
+
 }
